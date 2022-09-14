ce4410d6adfadaaa6276ed8018088c5a180b1248
PauloLeal <paulolealdev@gmail.com>
2022-09-14 01:09:11 -0300

    list channels

--- lib/services/firebase_notification_service.dart
+++ lib/services/firebase_notification_service.dart
@@ -104,18 +104,18 @@ class FirebaseNotificationService {
   }
 
   Future<List<Map<String, String>>> listChannels() async {
-    List<Map<String, String>> res = List.empty();
+    List<Map<String, String>> res = List.empty(growable: true);
 
     dynamic list = await _channel.invokeMethod("Notification.listChannels", {});
 
     for (var l in list) {
-      var m = Map<String, String>();
-      m["id"] = l["id"];
-      m["name"] = l["name"];
-      m["description"] = l["description"];
+      // var m = Map<String, String>();
+      // m["id"] = l["id"];
+      // m["name"] = l["name"];
+      // m["description"] = l["description"];
 
-      // Map.from(l);
-      res.add(m);
+      res.add(Map.from(l));
+      // res.add(m);
       // Log.debug("Channel id=${channel["id"]} name=${channel["name"]} description=${channel["description"]}");
     }
     return res;
