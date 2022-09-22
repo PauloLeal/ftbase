d85b2e160a8950c0aeffd495f50a3205a1c4961a
PauloLeal <paulolealdev@gmail.com>
2022-09-22 08:00:33 -0300

    exports

--- lib/ftbase.dart
+++ lib/ftbase.dart
@@ -1,20 +1,16 @@
-library ftbase;
-
-export 'services/firebase_analytics_service.dart' show FirebaseAnalyticsService;
-export 'services/firebase_dynamic_link_service.dart' show FirebaseDynamicLinkService;
-export 'services/firebase_functions_service.dart' show FirebaseFunctionsService;
-export 'services/firebase_login_service.dart' show FirebaseLoginService;
-export 'services/firebase_notification_service.dart' show FirebaseNotificationService;
-export 'services/firebase_service.dart' show FirebaseService;
-export 'services/firebase_storage_service.dart' show FirebaseStorageService, FileData;
-export 'services/geolocation_service.dart' show GeolocationService;
-export 'services/local_storage_service.dart' show LocalStorageService;
-export 'services/theme_service.dart' show ThemeService, ThemeType;
-export 'utils/exception_handler.dart' show ExceptionHandler;
-export 'utils/http_utils.dart' show HttpUtils;
-export 'utils/i18n.dart' show I18n;
-export 'utils/log.dart' show Log;
-export 'utils/regex_utils.dart' show RegexUtils;
-export 'utils/text_formatters.dart' show LowerCaseTextFormatter, UpperCaseTextFormatter;
-
-class Ftbase {}
+export 'services/firebase_analytics_service.dart';
+export 'services/firebase_dynamic_link_service.dart';
+export 'services/firebase_functions_service.dart';
+export 'services/firebase_login_service.dart';
+export 'services/firebase_notification_service.dart';
+export 'services/firebase_service.dart';
+export 'services/firebase_storage_service.dart';
+export 'services/geolocation_service.dart';
+export 'services/local_storage_service.dart';
+export 'services/theme_service.dart';
+export 'utils/exception_handler.dart';
+export 'utils/http_utils.dart';
+export 'utils/i18n.dart';
+export 'utils/log.dart';
+export 'utils/regex_utils.dart';
+export 'utils/text_formatters.dart';
