cef19fba03a2575dcbd39c2fea036f8758bd4de8
PauloLeal <paulolealdev@gmail.com>
2022-09-22 07:25:13 -0300

    export things

--- lib/ftbase.dart
+++ lib/ftbase.dart
@@ -1,1 +1,16 @@
-class Ftbase {}
+export 'services/firebase_analytics_service.dart';
+export 'services/firebase_dynamic_link_service.dart';
+export 'services/firebase_functions_service.dart';
+export 'services/firebase_login_service.dart';
+export 'services/firebase_notification_service.dart';
+export 'services/firebase_service.dart';
+export 'services/firebase_storage_service.dart';
+export 'services/geolocation_service.dart';
+export 'services/local_storage_service.dart';
+export 'services/theme_service.dart';
+export 'utils/exception_handler.dart';
+export 'utils/http_utils.dart';
+export 'utils/i18n.dart';
+export 'utils/log.dart';
+export 'utils/regex_utils.dart';
+export 'utils/text_formatters.dart';
