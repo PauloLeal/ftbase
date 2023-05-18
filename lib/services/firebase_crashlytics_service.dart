8ff644ad6771518ec8a8c396b1346c57f38d0a24
PauloLeal <paulolealdev@gmail.com>
2023-05-18 20:47:45 -0300

    feat: crashlytics

---
+++ lib/services/firebase_crashlytics_service.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+
+class FirebaseCrashlyticsService {
+  FirebaseCrashlyticsService._privateConstructor();
+  static final FirebaseCrashlyticsService instance = FirebaseCrashlyticsService._privateConstructor();
+
+  Future<void> initialize() async {
+    return _config(true);
+  }
+
+  void disableIf(bool condition) async {
+    _config(!condition);
+  }
+
+  void enableIf(bool condition) async {
+    _config(condition);
+  }
+
+  void _config(bool condition) async {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(condition);
+  }
+
+  /// Causes the app to crash (natively).
+  void crash() {
+    FirebaseCrashlytics.instance.crash();
+  }
+}
